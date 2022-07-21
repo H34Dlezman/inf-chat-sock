a3289092c68d3cacccb866f4bb059ce1a9916fac
headless <parsa@frty.de>
2022-07-21 06:06:32 +0200

    initial

---
+++ inf-chat-sock.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+const { Server } = require("socket.io");
+const io = new Server({
+        cors: {
+                origin: "http://127.0.0.1:5173"
+        }
+});
+
+messages = []
+function saveMessages() {
+  fs.writeFile('./messages.infc', JSON.stringify(messages), ()=>{})
+}
+try {
+  messages = JSON.parse(fs.readFileSync('./messages.infc', 'utf8'))
+} catch (err) {
+  saveMessages()
+  console.error(err);
+}
+
+io.on("connection", socket => {
+        console.log("client connected")
+	
+	socket.emit("msgs", messages)
+	
+        socket.on("msg", message => {
+		messages = messages.concat(message)
+		saveMessages()
+		io.emit("msgs", messages);
+        });
+});
+
+io.listen(5678);
+
